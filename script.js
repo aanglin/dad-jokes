fb4dc0bff915f59f1fdc4e9cc0c46b1dd95411dd
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-07-10 17:46:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var bottomRight = document.getElementById('bottomRight')
+$('#bottomRight').on('click', function(){
+    startJokes()
+})
+
+
+
+
+
+
+
+
+function startJokes(){
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2b4e532ae3msh54b5f70921ddc65p1b9fcejsn1929ae6001ba',
+		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+	}
+};
+
+fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
+.then(function (response) {
+           return response.json();
+          })
+         .then(function (data) {
+            console.log(data); 
+            
+            var getJoke = document.createElement('p')
+            getJoke.textContent='getJoke' + data.body
+            
+            
+            
+              
+            document.getElementById('jokes').append(JSON.stringify(getJoke));     
+        });
+    }         
+            
