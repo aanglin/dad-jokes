19f5b41dbcb4ef9c09a5f4accc19d0da866d4a27
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-07-11 16:31:17 -0500

    fixed buttons

--- script.js
+++ script.js
@@ -3,39 +3,44 @@ $('#bottomRight').on('click', function(){
     startJokes()
 })
 
-
-
-
-
-
-
-
 function startJokes(){
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2b4e532ae3msh54b5f70921ddc65p1b9fcejsn1929ae6001ba',
-		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-	}
-};
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2b4e532ae3msh54b5f70921ddc65p1b9fcejsn1929ae6001ba',
+            'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw%2Cracist', options)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data); 
+        
+        var getJoke = document.createElement('p')
+        getJoke.setAttribute('class','newJoke')
+        getJoke.textContent=data.setup
+        
+        var getPunch = document.createElement('p')
+        getPunch.setAttribute('class','newPunch')
+        getPunch.textContent=data.delivery
+        
+        document.getElementById('jokes').append(getJoke,getPunch);     
+        
+        
+        
+    });
+}         
 
-fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
-.then(function (response) {
-           return response.json();
-          })
-         .then(function (data) {
-            console.log(data); 
-            
-            var getJoke = document.createElement('p')
-            getJoke.textContent=data.body[0].setup
+//   var finish = document.getElementById('next-joke')
+//   $('#next-joke').on('click', function(){
+//       document.getElementById('jokes').value=''
+//       startJokes()
+     
+// })
 
-            var getPunch = document.createElement('p')
-            getPunch.textContent=data.body[0].punchline
-            
-            
-            
-              
-            document.getElementById('jokes').append(getJoke,getPunch);     
-        });
-    }         
-            
+
+
